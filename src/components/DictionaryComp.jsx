e9cd0a326e448f60e86a1f1b11acd1e2bc02340c
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-05 20:17:45 +0600

    UI+Logic coupled

--- src/components/DictionaryComp.jsx
+++ src/components/DictionaryComp.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
-import { FaSearch } from "react-icons/fa"
-import { FcSpeaker } from "react-icons/fc"
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { FcSpeaker } from "react-icons/fc";
 import axios from "axios";
 
 export default function DictionaryComp() {
@@ -10,46 +10,45 @@ export default function DictionaryComp() {
 
     //ASYNC FETCH
     function getMeaning() {
-        axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`).then((res) => {
-            setData(res.data[0]);
-        })
+        axios
+            .get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`)
+            .then((res) => {
+                setData(res.data[0]);
+            });
     }
 
     //HANDLERS
     function playAudio() {
-        let audio = new Audio(data.phonetics[0].audio)
+        let audio = new Audio(data.phonetics[0].audio);
         audio.play();
     }
 
     return (
         <>
             <div className="App">
-                <h1>Dictionary App</h1>
+                <h1>Simple Dictionary App</h1>
                 <div className="searchBox">
-                    <input type="text" placeholder='Search for word'
-                        onChange={0}
-                    />
-                    <button
-                        onClick={0}></button>
+                    <input type="text" placeholder="Search for word" onChange={(e) => { setSearchWord(e.target.value) }} />
+                    <button onClick={getMeaning}></button>
                     <FaSearch size="20px" />
                 </div>
-                {/* && */ (
-                    <div className="showResults">
-                        <h2>{0}
-                            <button onClick={0}>
+                {
+                    data && (<div className="showResults">
+                        <h2>
+                            {data.word}{" "}
+                            <button onClick={playAudio}>
                                 <FcSpeaker size="26px" />
                             </button>
                         </h2>
                         <h4>Parts of speech:</h4>
-                        <p>{0}</p>
+                        <p>{data.meanings[0].partOfSpeech}</p>
                         <h4>Definition:</h4>
-                        <p>{0}</p>
+                        <p>{data.meanings[0].definitions[0].definition}</p>
                         <h4>Example:</h4>
-                        <p>{0}</p>
-                    </div>
-
-                )}
+                        <p>{data.meanings[0].definitions[0].example || `n/a`}</p>
+                    </div>)
+                }
             </div>
         </>
-    )
+    );
 }
