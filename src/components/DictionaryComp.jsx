cd0b15d2b3b0bf5f18e446908f5813037d53a683
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-05 20:07:13 +0600

    State logic, fetch logic, handler logic added

--- src/components/DictionaryComp.jsx
+++ src/components/DictionaryComp.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
 import { FaSearch } from "react-icons/fa"
 import { FcSpeaker } from "react-icons/fc"
+import axios from "axios";
 
 export default function DictionaryComp() {
+    // State LOGIC
+    const [data, setData] = useState("");
+    const [searchWord, setSearchWord] = useState("");
+
+    //ASYNC FETCH
+    function getMeaning() {
+        axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`).then((res) => {
+            setData(res.data[0]);
+        })
+    }
+
+    //HANDLERS
+    function playAudio() {
+        let audio = new Audio(data.phonetics[0].audio)
+        audio.play();
+    }
+
     return (
         <>
             <div className="App">
@@ -16,8 +35,10 @@ export default function DictionaryComp() {
                 </div>
                 {/* && */ (
                     <div className="showResults">
-                        <h2>{0}<button onClick={0}><FcSpeaker size="26px" />
-                        </button>
+                        <h2>{0}
+                            <button onClick={0}>
+                                <FcSpeaker size="26px" />
+                            </button>
                         </h2>
                         <h4>Parts of speech:</h4>
                         <p>{0}</p>
